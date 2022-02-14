79614d186f439584eb55a6dd2779ad84a89e1df8
Maya Mircheva <may4eto@hotmail.com>
2022-02-14 18:22:01 +0100

    styled home page and empty cart page

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,9 @@
-// Put your application javascript here
+$(document).ready(function () {
+  $(document).on("change", "#variant-id", function () {
+    let variant_id = this.value,
+      variant_image = $(
+        '.single-product-image[data-variant="' + variant_id + '"]'
+      );
+    variant_image.show().siblings(":visible").hide();
+  });
+});
